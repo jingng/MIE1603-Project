3023aac5de200e7cf442ebf558ffd00ab5a733a8
Jin Gong <jin.gong@mail.utoronto.ca>
2025-03-09 12:38:16 -0400

    FUNCTIONAL MODEL

--- mip_model.py
+++ mip_model.py
@@ -2,129 +2,64 @@ import gurobipy as gp
 from gurobipy import GRB
 import networkx as nx
 import matplotlib.pyplot as plt
+import osmnx as ox
 
 import generate_instances
+import pickle
+import pandas as pd
+import time
 
-total_length = 75
+C = pd.read_pickle("instance-jess-min.pkl")
 
-n_nodes = 9
-nodes = [i for i in range(1, n_nodes+1)]
+# Get visited, profit, and penalty
+visited = {edge: C.edges[edge]['visited'] for edge in C.edges}
+profit = {edge: C.edges[edge]['profit'] for edge in C.edges}
+penalty = {edge: C.edges[edge]['penalty'] for edge in C.edges}
 
-# h_edge_lst = [(i,j) for i in nodes_lst for j in nodes_lst if j!=i] # all connected
+# Plot map 
+fig, ax = ox.plot.plot_graph(
+    C,
+    show=False,
+    close=False,
+    bgcolor="#333333",
+    edge_color="w",
+    edge_linewidth=0.3,
+    node_size=1,
+    node_color='r'
+)
+# plt.show()
 
-# (node i, node j)
-h_edge_lst = [
-            (1,2),
-            (1,6),
-            (1,7),
-            (2,3),
-            (2,7),
-            (2,8),
-            (3,4),
-            (3,8),
-            (4,5),
-            (4,8),
-            (5,6),
-            (5,9),
-            (6,7),
-            (7,8),
-            (7,9),
-            (8,9)
-            ]
+C = nx.DiGraph(C)
 
-h_edge_lst = h_edge_lst + [tuple(reversed(edge)) for edge in h_edge_lst] # adding reversed direction of edges
+# Parameters
+
+total_length=100
+
+edges, lengths = gp.multidict({edge:C.edges[edge]['length'] for edge in C.edges })
+nodes = C.nodes
 
 # Dictionary of connected nodes
-node_dict = {i:{j for _, j in h_edge_lst if _ == i } for i in nodes}
+# node_dict = {i:{(j,k) for _, j, k in edges if _ == i } for i in nodes}
+node_dict = {i:{j for _, j in edges if _ == i } for i in nodes}
 
-# Random
-g_edge_lst = [
-            (1,2),
-            (1,7),
-            (2,3),
-            (3,2),
-            (3,4),
-            (4,5),
-            (4,8),
-            (5,9),
-            (6,1),
-            (6,5),
-            (7,6),
-            (7,8),
-            (7,9), 
-            (8,7),
-            (8,2),
-            (8,3),
-            (9,7)
-            ]
+# Get visited, profit, and penalty
+visited = {edge: C.edges[edge]['visited'] for edge in C.edges} # historic penalty
+profit = {edge: C.edges[edge]['profit'] for edge in C.edges}
+penalty = {edge: C.edges[edge]['penalty'] for edge in C.edges}
 
-# Force subtours
-g_edge_lst = [
-            (1,2),
-            (1,6),
-            (2,1),
-            (2,3),
-            (3,2),
-            (3,4),
-            (4,3),
-            (4,5),
-            (5,4),
-            (5,6),
-            (6,5),
-            (6,1),
-            (7,8),
-            (7,9),
-            (8,7),
-            (8,9),
-            (9,7),
-            (9,8)
-            ]
+start_node_no = 8581834456
 
-# Define H (runnable paths)
-H = nx.DiGraph()
-H.add_nodes_from(nodes)
-H.add_edges_from(h_edge_lst)
-for edge in H.edges:
-    H.edges[edge]['length']=5
+# Checking profit
+profit = {edge: C.edges[edge]['profit'] for edge in C.edges if C.edges[edge]['profit'] >0}
 
-# list(H.edges(data=True))
-# H.edges[1,2]['distance']
 
-# Define G (citystrides)
-G = nx.DiGraph()
-G.add_nodes_from(nodes)
-G.add_edges_from(g_edge_lst)
-
-# 1. All roads have been ran, R = H
-# R = nx.DiGraph(H)
-
-# # Add passes
-# for edge in R.edges:
-#     R.edges[edge]['visited']=1
-
-# 2. No Roads have been ran, R is empty
-R = nx.DiGraph(H)
-for edge in R.edges:
-    R.edges[edge]['visited']=0
-
-# 3. Only H/G ran, R = H/G
-# R = nx.DiGraph(H)
-# for edge in R.edges:
-#    if edge in G.edges:
-#       R.edges[edge]["visited"]=0
-#    else:
-#       R.edges[edge]["visited"]=1
-
-# Define model
 m = gp.Model("mp")
-
-edges, lengths = gp.multidict({edge:H.edges[edge]['length'] for edge in H.edges })
-profit = {edge: (50 if edge in G.edges and R.edges[edge]['visited']==0 else 0) for edge in H.edges}
-penalty = {edge: (20 if edge in R.edges else 0) for edge in H.edges}
 
 x = m.addVars(edges, name="traversed", vtype=GRB.INTEGER) # number of times arc (i,j) is traversed
 y = m.addVars(edges, name="run", vtype=GRB.BINARY) # arc (i,j) is chosen to be ran, {0,1}
-z = m.addVars({(i,j) for i in nodes for j in nodes if i<j}, name='profitable_route', vtype=GRB.BINARY)
+
+z = m.addVars({(i,j) for i,j in edges if i< j}, name = 'profitable_route', vtype=GRB.BINARY)
+# z = m.addVars({(i,j) for i in nodes for j in nodes if i<j}, name='profitable_route', vtype=GRB.BINARY)
 
 ## Constraints
 
@@ -139,15 +74,15 @@ conn2_const = m.addConstrs((y[i,j]*10 >= x[i,j] for i in nodes for j in nodes if
 total_length_const = m.addConstr((x.prod(lengths)<=total_length), name = 'total_length')
 
 # Start at start node
-start_node = m.addConstrs((y.sum(1, j)>=1 for j in node_dict[1]), name='start_node')
+# start_node = m.addConstrs((y.sum(start_node_no, j)>=1 for j in node_dict[start_node_no]), name='start_node') ## this crashes the model, makes it infeasible
 
-# Set only one route as
+# Set only one route as profitable
 
 profit_1 = m.addConstrs((z[i,j] >= y[i,j] for i in nodes for j in nodes if i<=j and (i,j) in edges))
 profit_2 = m.addConstrs((z[i,j] >= y[j,i] for i in nodes for j in nodes if i<=j and (i,j) in edges))
 profit_3 = m.addConstrs((y[i,j] + y[j,i] >= z[i,j] for i in nodes for j in nodes if i<=j and (i,j) in edges))
 
-m.setObjective(z.prod(profit)-x.prod(penalty), GRB.MAXIMIZE)
+m.setObjective(z.prod(profit)-x.prod(penalty)-y.prod(visited), GRB.MAXIMIZE)
 
 def subtourelim(model, where):
     if where == GRB.Callback.MIPSOL:
@@ -182,19 +117,17 @@ def subtourelim(model, where):
         expr_arc = gp.quicksum(model._y[i,j] for i in subtour for j in subtour if i!=j and (i,j) in model._edges)
         model.cbLazy((expr_arc <= len(subtour) - 1))
 
-depot = 1
-
 # Set-up lazy constraints and call variables
-m.Params.LazyConstraints =1
+m.Params.LazyConstraints = 1
 m._x = x
 m._y = y
 m._edges = edges
-m._N = n_nodes
-m._depot = depot
+m._N = nodes
+m._depot = start_node_no
 m.optimize(subtourelim)
 
-m.write('optimal_run.lp')
-m.optimize()
+# m.write('optimal_run.lp')
+# m.optimize()
 
 for v in m.getVars():
     if v.x > 1e-6:
@@ -219,23 +152,6 @@ nx.draw(F, pos, with_labels=True, node_color='lightblue', edge_color='gray', arr
 plt.title("Optimal Run")
 plt.show()
 
-# Used to check sec
-# visited_edges = [edge for edge in edges if y[edge].x>0.05]
-# visited_nodes = set()
-# for edge in visited_edges:
-#     i,j = edge
-#     visited_nodes.update({i,j})
 
-# S = nx.Graph()
-# for i in visited_nodes:
-#    for j in visited_nodes:
-#       if (i,j) in edges:
-#         if i != j and y[(i,j)].x > 0.05:
-#             S.add_edge(i,j)
-
-# components = list(nx.connected_components(S))
-
-# for subtour in components:
-#    sum([y[(i,j)].x for i in subtour for j in subtour if i != j and (i,j) in edges])
-#    print(len(subtour) - 1)
+#######################################################################################
 
