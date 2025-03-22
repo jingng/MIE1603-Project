72d211a654ebee8234161049ed5d28cb301be601
Jin Gong <jin.gong@mail.utoronto.ca>
2025-03-22 12:33:31 -0400

    added test results from mip_model, mip_lb, mip_branching

--- mip_model.py
+++ mip_model.py
@@ -190,7 +190,7 @@ def solve_MIP(C, total_length, start_node_no, time_limit):
     # Create final graph solution
     print("saving graphs to file", end=" ", flush=True)
     
-    log_filepath = "test/"#"experiment_jess_1800/"
+    log_filepath = "final_test/"#"experiment_jess_1800/"
 
     with open(
         log_filepath+"sol_graph_" + instance + "_" + str(total_length) + ".pkl", "wb",
@@ -205,14 +205,21 @@ total_length_lst =[5000, 10000, 15000]
 instance_lst = ["instance-jess-min","instance-jess"]##["instance-jess-min"]#, "instance-jess"]#, "instance-jin.pkl"]
 
 start_node_no = 6813225352 # Start node for instance-jess-min
+# start_node_no = 394502562 # Start node for toronto
 # start_node_no = 1004361926 # New start node for instance-jess
 
 time_limit=1800
 
 for instance in instance_lst:
   for total_length in total_length_lst:
+    
+    # if 'jess' in instance:
+    #    start_node_no = 6813225352
+    
+    # if 'jin' in instance:
+    #    start_node_no = 394502562
 
-    log_filepath = "test/"#"experiment_jess_1800/"
+    log_filepath = "final_test/"#"experiment_jess_1800/"
     log_filename = log_filepath + instance + '_' + str(total_length) #+ str('_removed')
 
     print("reading instance file...")
